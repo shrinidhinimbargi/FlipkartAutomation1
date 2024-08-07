baccd80e6a3f56c0892fb79d938bd0d6ed46e391
= <nimbargishrinidhi@gmail.com>
2024-08-07 15:51:03 +0530

    ME_QA_XFLIPKART_SEARCH_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -9,11 +9,33 @@ import org.openqa.selenium.logging.LoggingPreferences;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+//package demo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.logging.LogType;
+import org.openqa.selenium.logging.LoggingPreferences;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+// import io.github.bonigarcia.wdm.WebDriverManager;
+import demo.wrappers.Wrappers;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
-
 
 // import io.github.bonigarcia.wdm.WebDriverManager;
 import demo.wrappers.Wrappers;
@@ -22,17 +44,17 @@ public class TestCases {
     ChromeDriver driver;
 
     /*
-     * TODO: Write your tests here with testng @Test annotation. 
-     * Follow `testCase01` `testCase02`... format or what is provided in instructions
+     * TODO: Write your tests here with testng @Test annotation.
+     * Follow `testCase01` `testCase02`... format or what is provided in
+     * instructions
      */
 
-     
     /*
-     * Do not change the provided methods unless necessary, they will help in automation and assessment
+     * Do not change the provided methods unless necessary, they will help in
+     * automation and assessment
      */
     @BeforeTest
-    public void startBrowser()
-    {
+    public void startBrowser() {
         System.setProperty("java.util.logging.config.file", "logging.properties");
 
         // NOT NEEDED FOR SELENIUM MANAGER
@@ -46,16 +68,62 @@ public class TestCases {
         options.setCapability("goog:loggingPrefs", logs);
         options.addArguments("--remote-allow-origins=*");
 
-        System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "build/chromedriver.log"); 
+        System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "build/chromedriver.log");
 
         driver = new ChromeDriver(options);
 
         driver.manage().window().maximize();
     }
 
+    @Test
+    public void testCase01() throws InterruptedException {
+        Wrappers homePage = new Wrappers(driver);
+
+        homePage.navigateToHome();
+        Thread.sleep(20000);
+
+        homePage.searchBox("Washing Machine");
+        Thread.sleep(4000);
+        WebElement button1 = driver.findElement(By.xpath("//div[text()='Popularity']"));
+        homePage.clickOn(button1);
+        Thread.sleep(4000);
+        homePage.countRating(4.0);
+        Thread.sleep(6000);
+    }
+
+    @Test
+    public void testCase02() throws InterruptedException {
+        Wrappers homePage = new Wrappers(driver);
+        homePage.navigateToHome();
+        Thread.sleep(20000);
+        homePage.searchBox("iPhone");
+        Thread.sleep(5000);
+        homePage.DiscountPercent(17);
+    }
+
+    @Test
+    public void testCase03() throws InterruptedException {
+        Wrappers homePage = new Wrappers(driver);
+        homePage.navigateToHome();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(40));
+        Thread.sleep(7000);
+        homePage.searchBox("Coffee Mug");
+        Thread.sleep(5000);
+
+        // wait.until(ExpectedConditions
+        // .presenceOfElementLocated(By.xpath("//div[text()='4★ &
+        // above']//preceding-sibling::input")));
+        WebElement element = driver
+                .findElement(By.xpath("(//div[@class='XqNaEv'])[1]"));
+        homePage.clickOn(element);
+        Thread.sleep(4000);
+
+        homePage.reviewCountsSort(5);
+
+    }
+
     @AfterTest
-    public void endTest()
-    {
+    public void endTest() {
         driver.close();
         driver.quit();
 
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -2,6 +2,7 @@ package demo.wrappers;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -13,10 +14,134 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+//package demo.wrappers;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+//import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.rmi.Remote;
 import java.time.Duration;
 
 public class Wrappers {
     /*
      * Write your selenium wrappers here
      */
+    WebDriver driver;
+
+    public Wrappers(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void navigateToHome() {
+        driver.get("https://www.flipkart.com/");
+    }
+
+    public void clickOn(WebElement element) {
+        element.click();
+    }
+
+    public void searchBox(String product) throws InterruptedException {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement element = wait.until(ExpectedConditions
+                .elementToBeClickable(By.xpath("//input[@name='q']")));
+        // Thread.sleep(4000);
+        element.clear();
+
+        Thread.sleep(8000);
+        element.sendKeys(product);
+        Thread.sleep(4000);
+        element.sendKeys(Keys.ENTER);
+    }
+
+    public void countRating(Double number) throws InterruptedException {
+        int count = 0;
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions
+                .presenceOfAllElementsLocatedBy(By.xpath("//div[@class='col col-7-12']//div[@class='XQDdHH']")));
+        List<WebElement> ratingList = driver
+                .findElements(By.xpath("//div[@class='col col-7-12']//div[@class='XQDdHH']"));
+        for (WebElement productRating : ratingList) {
+            String numbText = productRating.getText();
+            double numb = Double.parseDouble(numbText);
+            if (numb <= number) {
+                count++;
+            }
+        }
+        System.out.println("the washing machine rating less than or equal to " + number + " is : " + count);
+    }
+
+    public void DiscountPercent(int percentage) throws InterruptedException {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions
+                .presenceOfAllElementsLocatedBy(By.xpath("//div[@class='yKfJKb row']//div[@class='UkUFwK']/span")));
+        List<WebElement> percentageList = driver
+                .findElements(By.xpath("//div[@class='yKfJKb row']//div[@class='UkUFwK']/span"));
+        wait.until(ExpectedConditions
+                .presenceOfAllElementsLocatedBy(By.xpath("//div[@class='yKfJKb row']//div[@class='KzDlHZ']")));
+        List<WebElement> headingList = driver
+                .findElements(By.xpath("//div[@class='yKfJKb row']//div[@class='KzDlHZ']"));
+        for (int i = 0; i < percentageList.size(); i++) {
+            String percentageText = percentageList.get(i).getText();
+            String arr[] = percentageText.split(" ");
+            int num = Integer.parseInt(arr[0].replace("%", ""));
+            if (num > percentage) {
+                String heading = headingList.get(i).getText();
+                System.out.println("Phone title : " + heading);
+                System.out.println("Discount Percentage : " + percentageText);
+            }
+
+        }
+    }
+
+    public void reviewCountsSort(int item) throws InterruptedException {
+        int count = 0;
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions
+                .presenceOfAllElementsLocatedBy(
+                        By.xpath("//div[@class='slAVV4']//div[@class='_5OesEi afFzxY']/span[@class='Wphh3N']")));
+        List<WebElement> reviewsList = driver
+                .findElements(By.xpath("//div[@class='slAVV4']//div[@class='_5OesEi afFzxY']/span[@class='Wphh3N']"));
+        List<String> reviewTextList = new ArrayList<>();
+        for (WebElement reviewsListItem : reviewsList) {
+            String reviewText = reviewsListItem.getText();
+            reviewTextList.add(reviewText);
+        }
+        Collections.sort(reviewTextList, new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                // Remove parentheses and commas
+                String num1 = s1.replaceAll("[^\\d]", "");
+                String num2 = s2.replaceAll("[^\\d]", "");
+
+                // Compare the numeric values
+                return Integer.compare(Integer.parseInt(num2), Integer.parseInt(num1));
+            }
+        });
+        for (String reviewstText : reviewTextList) {
+            if (count < item) {
+                WebElement imgUrl = driver.findElement(
+                        By.xpath("//span[text()='" + reviewstText + "']//parent::div/preceding-sibling::a//img"));
+                String imgUrlText = imgUrl.getAttribute("src");
+                System.out.println("Url : " + imgUrlText);
+                WebElement title = driver.findElement(By.xpath("//span[text()='" + reviewstText
+                        + "']//parent::div[@class='_5OesEi afFzxY']//parent::div//a[@class='wjcEIp']"));
+                String titleText = title.getText();
+                System.out.println("title is : " + titleText);
+                System.out.println("The number of reviews are : " + reviewstText);
+                count++;
+            }
+        }
+    }
+
 }
